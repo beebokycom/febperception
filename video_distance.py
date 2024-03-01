847e4cc5e93ac3bbc174675cabdbc9be11c1c836
William <williamli1@berkeley.edu>
2024-02-29 22:31:27 -0800

    added webcam

--- video_distance.py
+++ video_distance.py
@@ -5,14 +5,10 @@ import cv2
 import os
 import ultralytics
 from ultralytics import YOLO
-
+import time
 import torch
 import utils
 import ssl
-
-# import model
-model_path = "/Users/william/Downloads/fa23_cone_segmentation.pt"
-model = YOLO(model_path)
 
 def bounding_box(img, model):
     # Convert the image to RGB format
@@ -24,7 +20,6 @@ def bounding_box(img, model):
     conf = results[0].boxes.conf.tolist()
     # Create copy of boxes
     image_with_boxes = img.copy()
-    print(conf)
     # Map the bounding boxes onto the image
     return boxes, conf
 
@@ -72,9 +67,6 @@ def display(cones,img,show):
         cv2.imshow("distances",img)
         cv2.waitKey(0)
     return img
-imgpath = "/Users/william/Desktop/Screenshot 2024-02-28 at 2.41.06 PM.png"
-# bboxes = bounding_box("",model,show=False)
-#print(cv2.imread("/Users/william/Desktop/Screenshot 2024-02-28 at 2.41.06 PM.png").shape[0])
 
 def process_video(input_video_path, output_video_path, model):
     cap = cv2.VideoCapture(input_video_path)
@@ -108,15 +100,51 @@ def process_video(input_video_path, output_video_path, model):
     # Close all OpenCV windows
     cv2.destroyAllWindows()
 
+def process_webcam(model):
+    cap = cv2.VideoCapture(0)  # 0 corresponds to the default webcam
+
+    while True:
+        # Capture one frame
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        # Apply find_distances and display functions
+        cones = find_distances(frame, model)
+        processed_frame = display(cones, frame, False)
+
+        # Display the processed frame
+        cv2.imshow("Live Results", processed_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # Pause for 1 second
+        time.sleep(0.1)
+
+    # Release video capture object
+    cap.release()
+
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
+
+# import model
+model_path = "/Users/william/Downloads/fa23_cone_segmentation.pt"
+model = YOLO(model_path)
+
+# Run process on webcam
+process_webcam(model)
+
 # Specify input and output video paths
 input_video_path = "/Users/william/Downloads/IMG_2163.MOV"
 output_video_path = "/Users/william/Downloads/FILTERED_IMG_2163 .MOV"
 
-# Call the process_video function
-process_video(input_video_path, output_video_path, model)
+# Run process on video
+#process_video(input_video_path, output_video_path, model)
 
-
-#testing on one image
+# Testing on single image
 # imgpath = "/Users/william/Desktop/Screenshot 2024-02-28 at 2.41.06 PM.png"
 # img = cv2.imread(imgpath)
 # cones = find_distances(img,model)
